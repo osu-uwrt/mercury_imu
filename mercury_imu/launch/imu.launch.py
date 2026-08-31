1ebf8391ef8652933caef64370c2deeefa1fec55
Effo12345 <4p7septipuses@gmail.com>
2026-08-30 23:35:14 -0400

    Get formatted lol

--- mercury_imu/launch/imu.launch.py
+++ mercury_imu/launch/imu.launch.py
@@ -6,13 +6,14 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     this_dir = get_package_share_directory('mercury_imu')
 
     return LaunchDescription([
         DeclareLaunchArgument(name="imu_port", default_value="/dev/ttyTHS0",
                               description="Port to connect over"),
-        DeclareLaunchArgument(name="imu_frame_id", default_value="mercury/imu_link", 
+        DeclareLaunchArgument(name="imu_frame_id", default_value="mercury/imu_link",
                               description="Frame to publish IMU data from"),
 
         Node(
@@ -22,8 +23,8 @@ def generate_launch_description():
             parameters=[
                 os.path.join(this_dir, 'config', 'vectornav.yaml'),
                 {
-                    "port" : LaunchConfiguration("imu_port"),
-                    "frame_id" : LaunchConfiguration("imu_frame_id")
+                    "port": LaunchConfiguration("imu_port"),
+                    "frame_id": LaunchConfiguration("imu_frame_id")
                 }
             ],
             respawn=True
